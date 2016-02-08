86b5bb80d53f0b8bb58f69afaa393a49121fe1d9
vokoshyv <ronsuij@gmail.com>
2016-02-08 11:21:31 -0800

    (feat) implements request-promise and cheerio scraping of craigslist

--- index.js
+++ index.js
@@ -5,9 +5,38 @@ var createCraigURL = function(car){
   return 'http://sfbay.craigslist.org/search/cta?is_paid=all&search_distance_type=mi&query=' + car;
 }
 
-var carURL = createCraigURL('mercedes');
+var carURL = createCraigURL('jeep');
 
-rp(carURL)
-.then(function(htmlString){
-  console.log(htmlString);
+var options = {
+  uri: carURL, 
+  transform: function(body){
+    return cheerio.load(body);
+  }
+}
+
+rp(options)
+.then(function($){
+
+  var cars = $('p.row');
+
+  for (var i = 0; i < cars.length; i++){
+
+    // car title
+    console.log("TITLE: ", cheerio(cars[i]).find('a.hdrlnk').text());
+
+    // car price
+    var price = cheerio(cars[i]).find('span.l2').find('span.price').text();
+    if (price.length !== 0){
+      console.log("PRICE: ", price);
+    } else {
+      console.log('PRICE:  NOT LISTED');
+    }
+
+    // car data posted
+    console.log("DATE: ", cheerio(cars[i]).find('time').attr('datetime'))
+
+    console.log('\n');
+
+  }
+
 })
