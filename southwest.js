38423a5452595786a511df453ac3f5311d481f9c
Ben Hsieh <Ben.T.Hsieh@gmail.com>
2016-02-10 16:18:21 -0800

    remove misc page and extra google horseman code

--- southwest.js
+++ southwest.js
@@ -10,14 +10,3 @@ horseman
   .text('#outbound_results')
   .log()
   .close();
-
-  // horseman
-  //   .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
-  //   .open('http://www.google.com')
-  //   .type('input[name="q"]', 'github')
-  //   .click("button:contains('Google Search')")
-  //   .keyboardEvent("keypress",16777221)
-  //   .waitForSelector("div.g")
-  //   .count("div.g")
-  //   .log() // prints out the number of results
-  //   .close();
